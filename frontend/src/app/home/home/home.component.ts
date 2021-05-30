17f4494c10ead68fd42d37ad5bdcc57a46c63457
PriyankaMali-13 <67782275+PriyankaMali-13@users.noreply.github.com>
2021-05-30 17:06:32 +0530

    changes done in UI

--- frontend/src/app/home/home/home.component.ts
+++ frontend/src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,33 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private _router: Router) {}
 
   ngOnInit(): void {}
 
-  imageObject = [
-    {
-      image:
-        'https://whnt.com/wp-content/uploads/sites/20/2019/10/s112160040.jpg?w=2560&h=1440&crop=1',
-      thumbImage:
-        'https://whnt.com/wp-content/uploads/sites/20/2019/10/s112160040.jpg?w=2560&h=1440&crop=1',
-    },
-    {
-      image: 'https://wallpaperaccess.com/full/1496231.jpg',
-      thumbImage: 'https://wallpaperaccess.com/full/1496231.jpg',
-    },
-    {
-      image:
-        'https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?size=626&ext=jpg',
-      thumbImage:
-        'https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?size=626&ext=jpg',
-    },
-
-    {
-      image:
-        'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWxDHD?ver=5cd6&q=90&m=2&h=2147483647&w=2147483647&b=%23FFFFFFFF&aim=true',
-      thumbImage:
-        'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWxDHD?ver=5cd6&q=90&m=2&h=2147483647&w=2147483647&b=%23FFFFFFFF&aim=true',
-    },
-  ];
+  
 }
